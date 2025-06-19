3f6b02b919035ccc2c73bf162c5dfdbe533af626
pknowledge <shubhamsajjan263@gmail.com>
2025-06-19 09:53:37 +0530

    12

--- Program13.java
+++ Program13.java
@@ -1,7 +1,25 @@
 public class Program13 {
+    public static boolean issorted(int arr[]){
+
+        for(int i=0;i< arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+               return false;
+            }
+
+        }
+        return true;
+
+
+    }
+
+    //to finnd array is sorted or not
     public static void main(String[] args) {
+int[] arr= {10,20,30,40,50};
 
 
+
+boolean sorted=issorted(arr);
+        System.out.println(sorted);
 
 
 
