8d009271beaa62a5de96b605b8de68e523fd26c5
pknowledge <shubhamsajjan263@gmail.com>
2025-06-19 10:24:04 +0530

    12

--- Program15.java
+++ Program15.java
@@ -1,13 +1,30 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Program15 {
-    public static int[] antoclockwise
+
+    // Method to rotate array elements to the left by 1 position (anticlockwise)
+    public static int[] antoclockwise(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // Shift each element to the previous index
+            arr[i - 1] = arr[i];
+        }
+        return arr;  // The last element will be updated in main()
+    }
 
     public static void main(String[] args) {
-        int arr[] = {10,20,30,40,50};
+        // Declare and initialize the array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Store the first element temporarily (it will be moved to the end)
         int temp = arr[0];
-        arr[arr.length-1]=temp;
-        System.out.println(Arrays.toString(arr));
+
+        // Call the method to shift elements to the left
+        antoclockwise(arr);
+
+        // Place the first element at the end of the array
+        arr[arr.length - 1] = temp;
+
+        // Print the final rotated array
+        System.out.println(Arrays.toString(arr));  // Output: [20, 30, 40, 50, 10]
     }
 }
