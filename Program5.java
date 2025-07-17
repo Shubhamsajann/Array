cb6881188defd7a54607be380e925b946b1a043c
shubham sajjan <175342989+Shubhamsajann@users.noreply.github.com>
2025-07-17 13:39:34 +0530

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,17 +1,23 @@
 public class Program5 {
-    //program to print element present int even index
+    
+    //program to print element present in even index
+    
     public static void main(String[] args) {
 
         // Declare and initialize array
         int[] arr ={12,23,19,10,5,90};
+        
         // Loop through each element
         for(int i=0;i<arr.length;i++){
+            
             // Check if the element is even
             if(i%2 ==0){
                 // Print the even element
                 System.out.print(arr[i]+" ");
             }
+            
         }
+        
     }
 }
 /*
